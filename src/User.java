9b3abc772303244bd57cccd294a2a89fce4738ce
LukasEliasson <07lukeli@gafe.molndal.se>
2025-12-12 14:05:04 +0100

    Created basic user class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,39 @@
 public class User {
+
+    String username;
+    String password;
+    BookCollection loanedBooks;
+
+    public User(String username, String password) {
+
+        this.username = username;
+        this.password = password;
+        this.loanedBooks = new BookCollection();
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addBook(Book book) {
+        this.loanedBooks.addBook(book);
+    }
+
+    public void removeBook(Book book) {
+        this.loanedBooks.removeBook(book);
+    }
+
 }
